fa05424287764fb1919bb3bfa917c5de07c458d6
Eclipse <43897374+Eclipsem@users.noreply.github.com>
2021-03-08 17:58:11 -0500

    Update admin.php

--- dashboard/admin/admin.php
+++ dashboard/admin/admin.php
@@ -8,27 +8,13 @@
  <?php
     $clientip = $_SERVER['REMOTE_ADDR']
     $whitelistedip = '98.180.85.44'
-    #define('SITE_URL', 'https://www.staude.ml/dashboard/admin/index.php');
-    
-    #if(!defined('SITE_URL')) {
-    #header($_SERVER["SERVER_PROTOCOL"] . " 403 Forbidden");
-    #exit;
-    #}
-    
+
     if($clientip == $whitelistedip) {
     print("woooo works lolol"
     } else {
       header($_SERVER["SERVER_PROTOCOL"] . " 403 Forbidden");
       exit;
     }
-    
-        
-    #function CheckIPAddress()
-    #{
-     #   $allowedip = '98.180.85.44';
-     #   $ip = $_SERVER['REMOTE_ADDR'];
-     #   return ($ip == $allowedip);
-    #}
 ?>
     </body>
 </html>
