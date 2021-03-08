548d709714db468570f62e64c99974307bcfafe2
Eclipse <43897374+Eclipsem@users.noreply.github.com>
2021-03-08 17:02:57 -0500

    Update index.php

--- dashboard/admin/index.php
+++ dashboard/admin/index.php
@@ -6,10 +6,16 @@
     
     <body>
  <?php
-    function CheckAccess()
+    define('SITE_URL', 'https://www.staude.ml/dashboard/admin/index.php');
+    
+    if(!defined('SITE_URL')) {
+    header($_SERVER["SERVER_PROTOCOL"] . " 403 Forbidden");
+    exit;
+    }
+        
+    function CheckIPAddress()
     {
-        //allowed IP. Change it to your static IP
-        $allowedip = '127.0.0.1';
+        $allowedip = '98.180.85.44';
         $ip = $_SERVER['REMOTE_ADDR'];
         return ($ip == $allowedip);
     }
